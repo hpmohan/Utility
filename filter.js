4206d8e426b9c8d326a9151e502f944f3a100368
Hareesh P Mohan <hareeshpmohan@gmail.com>
2018-02-14 18:33:20 +0530

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,7 +1,5 @@
 //--- Remove selected job reason from picklist
 var laborTypeList = component.get("v.laborTypeList");
-//var selectedLaborType = component.get("v.selectedLaborType");
-
 var newLaborTypeList = laborTypeList.filter(function(a){
     if(a !== selectedLaborType){
         return a
